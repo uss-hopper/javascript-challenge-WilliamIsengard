858a33f60c3ade5c759de91c3e931035cf8342b0
William Isengard <william.isengard@gmail.com>
2019-02-21 22:04:37 -0700

    Created mouseover event and working on onkeypress event.

--- script.js
+++ script.js
@@ -1,19 +1,82 @@
+//Change Paragraph 1 Color//
+
 function button1() {
 	window.onclick = document.getElementById("paragraph1").style.color = "#f44e42";
 }
+
+//Change Paragraph 2 Color//
 
 function button2() {
 	window.onclick = document.getElementById("paragraph2").style.color = "#41f4d6";
 }
 
+//Change Paragraph 3 Color//
+
 function button3() {
 	window.onclick = document.getElementById("paragraph3").style.color = "#d1f442";
 }
+
+//Change Font Type//
 
 function button4() {
 	window.onclick = document.getElementById("allParagraphs").style.fontFamily = "Comic Sans MS", cursive, sans-serif;
 }
 
+//Change Paragraph1 Color On Mouseover//
+
+function mouseover1() {
+	window.onmouseover = document.getElementById("paragraph1").style.color = "#5642f4";
+}
+
+//Change Paragraph1 Color On Mouseover//
+
+function mouseover2() {
+	window.onmouseover = document.getElementById("paragraph2").style.color = "#d1f442";
+}
+
+//Change Paragraph1 Color On Mouseover//
+
+function mouseover3() {
+	window.onmouseover = document.getElementById("paragraph3").style.color = "#5642f4";
+}
+
+//Word Highlighter//
+
+function highlight(word) {
+	var inputText = document.getElementById("highlightWord");
+	var innerHTML = inputText.innerHTML;
+	var index = innerHTML.indexOf(word);
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0,index) + "<span class='highlighter'>" + innerHTML.substring(index,index+word.length) + "</span>" + innerHTML.substring(index + word.length);
+		inputText.innerHTML = innerHTML;
+	}
+}
+
+// //Erase Letters On Keyboard Clicks//
+//
+// function erase() {
+// 	window.onkeydown = document.getElementById("erase") {
+// 	switch (event.keyCode) {
+// 		case 69:
+// 			$(".e").css("display", "none");
+// 			break;
+// 		case 78:
+// 			$(".n").css("display", "none");
+// 			break;
+// 		case 71:
+// 			$(".g").css("display", "none");
+// 			break;
+// 		case 65:
+// 			$(".a").css("display", "none");
+// 			break;
+// 		case 71:
+// 			$(".g").css("display", "none");
+// 			break;
+// 		case 69:
+// 			$(".e").css("display", "none");
+// 			break;
+// 	}
+// }};
 
 // document.getElementById("button1").addEventListener("click", changeColor1);
 //
@@ -32,13 +95,3 @@ function button4() {
 // function changeColor3() {
 // 	document.getElementById("paragraph3").style.color = "#9242f4";
 // }
-
-function highlight(word) {
-	var inputText = document.getElementById("highlightWord");
-	var innerHTML = inputText.innerHTML;
-	var index = innerHTML.indexOf(word);
-	if (index >= 0) {
-		innerHTML = innerHTML.substring(0,index) + "<span class='highlighter'>" + innerHTML.substring(index,index+word.length) + "</span>" + innerHTML.substring(index + word.length);
-		inputText.innerHTML = innerHTML;
-	}
-}
