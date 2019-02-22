442c5e63fdc34613200850a102719afc621e4839
William Isengard <william.isengard@gmail.com>
2019-02-21 20:55:21 -0700

    Created JS functions to change font colors and font type on button click.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const ipsum = [{"These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent."},{"We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal."},{"Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam."}];
+function button1() {
+	window.onclick = document.getElementById("paragraph1").style.color = "#f44e42";
+}
 
-function lookInScriptJsLine3() {
-	let renderedIpsum = ipsum.map((index) => {
-		return `
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">${index.title}</h5>
-        <p class="card-text">${index.body}</p>
-      </div>
-  </div>
-    `
-	});
+function button2() {
+	window.onclick = document.getElementById("paragraph2").style.color = "#41f4d6";
+}
 
-	let target = document.getElementById('target');
-	console.log(target);
-	target.innerHTML = renderedIpsum.join("\r\n");
+function button3() {
+	window.onclick = document.getElementById("paragraph3").style.color = "#d1f442";
+}
+
+function button4() {
+	window.onclick = document.getElementById("allParagraphs").style.fontFamily = "Comic Sans MS", cursive, sans-serif;
 }
 
 
-
+// document.getElementById("button1").addEventListener("click", changeColor1);
+//
+// function changeColor1() {
+// 	document.getElementById("paragraph1").style.color = "#f44e42";
+// }
+//
+// document.getElementById("button2").addEventListener("click", changeColor2);
+//
+// function changeColor2() {
+// 	document.getElementById("paragraph2").style.color = "#41f4d6";
+// }
+//
+// document.getElementById("button3").addEventListener("click", changeColor3);
+//
+// function changeColor3() {
+// 	document.getElementById("paragraph3").style.color = "#9242f4";
+// }
